4f6a57d4583cb7d32c22d4ef5dc22d5fa5c2c8cb
damianstone <damianstonedev@gmail.com>
2020-06-10 00:09:46 +0100

    configuration of babel and webpack

---
+++ VideoProject/src/index.js
@@ -0,0 +1,17 @@
+// ESTE ES EL ARCHIVO PRINCIPAL DONDE SE JUNTAN LOS COMPONENTES Y SE LLEVA HACIA EL DOM = WEB  
+
+import React from 'react';
+import ReactDOM from 'react';
+import HelloWorld from './components/HelloWorld';
+
+ReactDOM.render(<HelloWorld />, document.getElementById(app)); // metodo render es lo que lleva todo al navegador
+
+
+
+
+
+
+
+
+
+
